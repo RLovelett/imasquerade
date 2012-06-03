b1aa987514d1116b828e070392452ea0b2aa9021
Ryan Lovelett <ryan@wahvee.com>
2012-06-03 12:28:54 -0500

    Actually test the example in the README.

--- test/test_imasquerade.rb
+++ test/test_imasquerade.rb
@@ -1,7 +1,10 @@
 require 'helper'
 
 class TestImasquerade < Test::Unit::TestCase
-  should "probably rename this file and start testing for real" do
-    flunk "hey buddy, you should probably rename this file and start testing for real"
+  should "perform the example in the README" do
+    uri = "http://itunes.apple.com/us/podcast/the-f-u-sports-talk-podcast/id356478903"
+    proper_response = "http://www.urannonline.com/Collection_Site/The_F_U_Sports_Talk_Podcast/rss.xml"
+    response = Imasquerade::Extractor.parse_itunes_uri uri
+    assert_equal(proper_response, response)
   end
 end
